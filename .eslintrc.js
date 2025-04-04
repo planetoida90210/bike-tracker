06b3d6d0bac852995bb59d974f140988cc10debd
planetoida90210 <galeriakoloru90210@gmail.com>
2025-04-04 11:23:28 +0200

    fixing errors

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  rules: {
+    "no-restricted-globals": "off",
+  },
+};
